1a8ec7be99a90fb239ed67f0a79732f6eafa6af2
thyagostall <thstall@gmail.com>
2016-04-06 09:45:03 -0300

    Incremental changes in ChordView

--- app/src/main/java/com/thyago/chordi/practice/ChordView.java
+++ app/src/main/java/com/thyago/chordi/practice/ChordView.java
@@ -5,12 +5,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
  * Created by thyago on 4/3/16.
  */
 public class ChordView extends View {
+
+    private static final String LOG_TAG = ChordView.class.getSimpleName();
+    private static final int STRING_QTY = 6;
+    private static final int STRING_WIDTH = 6;
+    private static final int FINGER_RADIUS = 40;
+    private static final int NUT_HEIGHT = 20;
+    private static final int FRET_QTY = 4;
+    private static final int MUTED_STRINGS_HEIGHT = 50;
 
     private Paint mPaintChordView;
 
@@ -31,14 +40,60 @@ public class ChordView extends View {
 
     private void init(AttributeSet attrs, int defStyleAttr) {
         mPaintChordView = new Paint();
-        mPaintChordView.setColor(Color.RED);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawLine(0, 0, getWidth(), getHeight(), mPaintChordView);
+        setBackgroundColor(Color.CYAN);
+
+        int neckWidth = getWidth() - 2 * FINGER_RADIUS;
+        int distance = neckWidth / (STRING_QTY - 1);
+        int marginLeft = FINGER_RADIUS;
+
+        int marginRight = getWidth() - FINGER_RADIUS;
+
+        int marginTop = NUT_HEIGHT;
+        int fretDistance = getHeight() / (FRET_QTY + 1);
+
+        Log.d(LOG_TAG, "Distance: " + distance);
+        Log.d(LOG_TAG, "Width: " + getWidth());
+
+        drawNut(canvas, FINGER_RADIUS, 0, getWidth() - 2 * FINGER_RADIUS, NUT_HEIGHT);
+
+        mPaintChordView.setColor(Color.GRAY);
+        mPaintChordView.setStrokeWidth(STRING_WIDTH);
+
+        for (int i = 0; i < STRING_QTY - 1; i++) {
+            marginTop += fretDistance;
+            canvas.drawLine(marginLeft, marginTop, marginRight, marginTop, mPaintChordView);
+        }
+
+        mPaintChordView.setStrokeWidth(STRING_WIDTH);
+
+        for (int i = 0; i < STRING_QTY; i++) {
+            mPaintChordView.setColor(Color.BLACK);
+            canvas.drawLine(marginLeft, 0, marginLeft, getHeight() - MUTED_STRINGS_HEIGHT, mPaintChordView);
+            drawMutedStringDiagram(canvas, marginLeft, getHeight() - MUTED_STRINGS_HEIGHT, MUTED_STRINGS_HEIGHT);
+            marginLeft += distance;
+        }
+    }
+
+    private void drawMutedStringDiagram(Canvas canvas, int centerX, int startY, int length) {
+        mPaintChordView.setColor(Color.RED);
+        canvas.drawRect(centerX - length / 2, startY, centerX + length / 2, startY + length, mPaintChordView);
+        mPaintChordView.setColor(Color.BLUE);
+        canvas.drawCircle(centerX, startY + length / 2, length / 3, mPaintChordView);
+        mPaintChordView.setColor(Color.GREEN);
+        canvas.drawLine(centerX - length / 2, startY, centerX + length / 2, startY + length, mPaintChordView);
+        canvas.drawLine(centerX + length / 2, startY, centerX - length / 2, startY + length, mPaintChordView);
+    }
+
+    private void drawNut(Canvas canvas, int startX, int startY, int width, int height) {
+        mPaintChordView.setColor(Color.BLACK);
+        mPaintChordView.setStrokeWidth(STRING_WIDTH);
+        canvas.drawRect(startX, startY, width + startX, height + startY, mPaintChordView);
     }
 
 }
